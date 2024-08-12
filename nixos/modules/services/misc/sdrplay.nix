0bcfebaf7f0c815a22ebf4377cf92f5849bf5eeb
Luke Sandell <lasandell@gmail.com>
2024-08-11 19:11:53 -0500

    add services.sdrplayApi.soapySdrPlugin.enable option

--- nixos/modules/services/misc/sdrplay.nix
+++ nixos/modules/services/misc/sdrplay.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 with lib;
+let 
+  cfg = config.services.sdrplayApi;
+in
 {
   options.services.sdrplayApi = {
     enable = mkOption {
@@ -9,27 +12,58 @@ with lib;
         Whether to enable the SDRplay API service and udev rules.
 
         ::: {.note}
-        To enable integration with SoapySDR and GUI applications like gqrx create an overlay containing
-        `soapysdr-with-plugins = super.soapysdr.override { extraPackages = [ super.soapysdrplay ]; };`
+        See the `services.sdrplayApi.soapySdrPlugin.enable` option for information on enabling the
+        SoapySDR plugin for applications that require it.
         :::
       '';
       type = lib.types.bool;
     };
-  };
 
-  config = mkIf config.services.sdrplayApi.enable {
-    systemd.services.sdrplayApi = {
-      description = "SDRplay API Service";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.sdrplay}/bin/sdrplay_apiService";
-        DynamicUser = true;
-        Restart = "on-failure";
-        RestartSec = "1s";
+    soapySdrPlugin = {
+      enable = mkOption {
+        default = cfg.enable;
+        defaultText = literalExpression "config.services.sdrplayApi.enable";
+        example = true;
+        description = ''
+          Whether to enable the SDRplay plugin for SoapySDR.
+
+          If enabled, the `SOAPY_SDR_PLUGIN_PATH` environment variable will be set to the path of the
+          SoapySDR plugin, allowing applications like Gqrx and CubicSDR to find it. You will need to
+          to log out of your current session for the new environment variable to take effect.
+
+          ::: {.note}
+          Previously it was suggested to create an overlay containing
+          `soapysdr-with-plugins = super.soapysdr.override { extraPackages = [ super.soapysdrplay ]; }`
+          to enable the plugin. However, this causes packages that depend on SoapySDR to be frequently
+          rebuilt, so it is no longer recommended.
+          :::
+        '';
+        type = lib.types.bool;
       };
     };
-    services.udev.packages = [ pkgs.sdrplay ];
-
   };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      systemd.services.sdrplayApi = {
+        description = "SDRplay API Service";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStart = "${pkgs.sdrplay}/bin/sdrplay_apiService";
+          DynamicUser = true;
+          Restart = "on-failure";
+          RestartSec = "1s";
+        };
+      };
+  
+      services.udev.packages = [ pkgs.sdrplay ];
+    })
+
+    (mkIf cfg.soapySdrPlugin.enable {
+      environment.variables = {
+        SOAPY_SDR_PLUGIN_PATH = "${pkgs.soapysdrplay}/${pkgs.soapysdr.passthru.searchPath}";
+      };
+    })
+  ];
 }
